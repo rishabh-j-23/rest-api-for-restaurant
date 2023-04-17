db44f925b12499b58b88d98b07ca2fd4b0c1825f
rishabh-j-23 <rjain23112002@gmail.com>
2023-04-17 14:33:50 +0530

    facebook OAuth2

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/course'
+    'mongoUrl' : 'mongodb://localhost:27017/course',
+    'facebook' : {
+        clientId: '550851177133298',
+        clientSecret: 'd70ff4ee504b71d217430f3f6ee3c027'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -66,6 +66,17 @@ router.post('/login', cors.corsWithOPtions, passport.authenticate('local'), (req
 	});
 });
 
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res, next) => {
+	if (req.user) {
+		var token = authenticate.getToken({_id: req.user._id});
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'application/json');
+		res.json({
+			"success": true,
+			"token": token,
+		});
+	}
+});
 
 
 module.exports = router;
